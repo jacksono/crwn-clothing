2691c99e0de8a48c57eb82f278f2f31efe4fb654
Jackson Onyango <jackson.onyango@gmail.com>
2020-05-27 13:36:04 +0300

    Add routing with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component'
+
+import './App.css';
+
+const Hats = () => (
+  <h1> Them hats</h1>
+)
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop/hats' component={Hats} />
+      </Switch>
     </div>
   );
 }
