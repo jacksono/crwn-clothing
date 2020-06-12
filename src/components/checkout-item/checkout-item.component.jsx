0278ede22875573966710d96527211b2ae9c1d27
Jackson Onyango <jackson.onyango@gmail.com>
2020-06-12 06:05:55 +0300

    Add remove item from cart functionality

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,17 +1,29 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import { removeItemFromCart } from "../../redux/cart/cart.actions";
 
 import "./checkout-item.styles.scss";
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, quantity, price } }) => (
-  <div className="checkout-item">
-    <div className="image-container">
-      <img src={imageUrl} alt="image" />
+const CheckoutItem = ({ cartItem, removeItem }) => {
+  const { name, imageUrl, quantity, price } = cartItem;
+  return (
+    <div className="checkout-item">
+      <div className="image-container">
+        <img src={imageUrl} alt="image" />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">{quantity}</span>
+      <span className="price">{price}</span>
+      <div className="remove-button" onClick={() => removeItem(cartItem)}>
+        &#10005;
+      </div>
     </div>
-    <span className="name">{name}</span>
-    <span className="quantity">{quantity}</span>
-    <span className="price">{price}</span>
-    <div className="remove-item">&#10005;</div>
-  </div>
-);
+  );
+};
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+  removeItem: item => dispatch(removeItemFromCart(item))
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
